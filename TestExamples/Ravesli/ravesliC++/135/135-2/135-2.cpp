56cdb38665ca4237a2b0e8ae12f5eb23eddbbd17
Viilture <53421674+Viilture@users.noreply.github.com>
2022-06-29 21:50:22 +0300

    Ravesli 130-135 learning

---
+++ TestExamples/Ravesli/ravesliC++/135/135-2/135-2.cpp
@@ -0,0 +1,49 @@
+﻿// 135-2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include "pch.h"
+#include <iostream>
+
+class Something
+{
+private:
+    double m_a, m_b, m_c;
+public:
+    Something(double a = 0.0, double b = 0.0, double c = 0.0) : m_a(a), m_b(b), m_c(c) {}
+
+    Something operator- () const
+    {
+        return Something(-m_a, -m_b, -m_c);
+    }
+
+    bool operator !() const
+    {
+        return (m_a == 0.0 && m_b == 0.0 && m_c == 0.0);
+    }
+
+    template <class T>
+    T operator + ()
+    {
+        return m_a+m_b;
+    }
+
+    double getA() { return m_a; }
+    double getB() { return m_b; }
+    double getC() { return m_c; }
+};
+
+int main()
+{
+    Something som;
+
+    if (!som)
+        std::cout << "Something is null.\n";
+    else
+        std::cout << "Something is not null.\n";
+
+
+
+    return 0;
+}
+
+
